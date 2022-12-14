f9f004a758f03f4012ce712d1cc0d74432686b23
SiBen10 <benjaminkn805@gmail.com>
2022-12-14 13:22:42 +0100

    pyproject

---
+++ setup.py
@@ -0,0 +1,40 @@
+import os
+
+from setuptools import find_packages, setup
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(current_dir, "README.rst")) as f:
+    long_desc = f.read()
+
+setup(
+    name="monty",
+    packages=find_packages(),
+    version="2022.9.9",
+    extras_require={
+        "yaml": ["ruamel.yaml"],
+    },
+    package_data={},
+    author="Shyue Ping Ong",
+    author_email="ongsp@ucsd.edu",
+    maintainer="Shyue Ping Ong",
+    url="https://github.com/materialsvirtuallab/monty",
+    license="MIT",
+    description="Monty is the missing complement to Python.",
+    long_description=long_desc,
+    keywords=["monty"],
+    python_requires=">=3.5",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
---
+++ tasks.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+
+"""
+Deployment file to facilitate releases of monty.
+"""
+
+
+import datetime
+import glob
+import json
+import os
+import re
+
+import requests
+from invoke import task
+
+from monty import __version__ as ver
+from monty.os import cd
+
+__author__ = "Shyue Ping Ong"
+__copyright__ = "Copyright 2012, The Materials Project"
+__version__ = "0.1"
+__maintainer__ = "Shyue Ping Ong"
+__email__ = "shyue@mit.edu"
+__date__ = "Apr 29, 2012"
+
+NEW_VER = datetime.datetime.today().strftime("%Y.%-m.%-d")
+
+
+@task
+def make_doc(ctx):
+    with cd("docs_rst"):
+        ctx.run("sphinx-apidoc --separate -d 6 -o . -f ../monty")
+        for f in glob.glob("*.rst"):
+            if f.startswith("monty") and f.endswith("rst"):
+                newoutput = []
+                suboutput = []
+                subpackage = False
+                with open(f) as fid:
+                    for line in fid:
+                        clean = line.strip()
+                        if clean == "Subpackages":
+                            subpackage = True
+                        if not subpackage and not clean.endswith("tests"):
+                            newoutput.append(line)
+                        else:
+                            if not clean.endswith("tests"):
+                                suboutput.append(line)
+                            if clean.startswith("monty") and not clean.endswith("tests"):
+                                newoutput.extend(suboutput)
+                                subpackage = False
+                                suboutput = []
+
+                with open(f, "w") as fid:
+                    fid.write("".join(newoutput))
+        ctx.run("make html")
+
+        # ctx.run("cp _static/* ../docs/html/_static")
+
+    with cd("docs"):
+        ctx.run("cp -r html/* .")
+        ctx.run("rm -r html")
+        ctx.run("rm -r doctrees")
+        ctx.run("rm -r _sources")
+
+        # This makes sure monty.org works to redirect to the Gihub page
+        # ctx.run("echo \"monty.org\" > CNAME")
+        # Avoid the use of jekyll so that _dir works as intended.
+        ctx.run("touch .nojekyll")
+
+
+@task
+def update_doc(ctx):
+    ctx.run("git pull", warn=True)
+    make_doc(ctx)
+    ctx.run("git add .", warn=True)
+    ctx.run('git commit -a -m "Update dev docs"', warn=True)
+    ctx.run("git push", warn=True)
+
+
+@task
+def test(ctx):
+    ctx.run("pytest")
+
+
+@task
+def setver(ctx):
+    ctx.run(f'sed s/version=.*,/version=\\"{ver}\\",/ setup.py > newsetup')
+    ctx.run("mv newsetup setup.py")
+
+
+@task
+def release_github(ctx):
+    with open("docs_rst/changelog.rst") as f:
+        contents = f.read()
+    toks = re.split(r"\-+", contents)
+    desc = toks[1].strip()
+    toks = desc.split("\n")
+    desc = "\n".join(toks[:-1]).strip()
+    payload = {
+        "tag_name": "v" + NEW_VER,
+        "target_commitish": "master",
+        "name": "v" + NEW_VER,
+        "body": desc,
+        "draft": False,
+        "prerelease": False,
+    }
+
+    response = requests.post(
+        "https://api.github.com/repos/materialsvirtuallab/monty/releases",
+        data=json.dumps(payload),
+        headers={"Authorization": "token " + os.environ["GITHUB_RELEASES_TOKEN"]},
+    )
+    print(response.text)
+
+
+@task
+def commit(ctx):
+    ctx.run('git commit -a -m "v%s release"' % NEW_VER, warn=True)
+    ctx.run("git push", warn=True)
+
+
+@task
+def set_ver(ctx):
+    with open("monty/__init__.py") as f:
+        contents = f.read()
+        contents = re.sub(r"__version__ = .*\n", '__version__ = "%s"\n' % NEW_VER, contents)
+
+    with open("monty/__init__.py", "w") as f:
+        f.write(contents)
+
+    with open("setup.py") as f:
+        contents = f.read()
+        contents = re.sub(r"version=([^,]+),", 'version="%s",' % NEW_VER, contents)
+
+    with open("setup.py", "w") as f:
+        f.write(contents)
+
+
+@task
+def release(ctx):
+    set_ver(ctx)
+    test(ctx)
+    update_doc(ctx)
+    commit(ctx)
+    release_github(ctx)
+    ctx.run("python setup.py sdist bdist_wheel", warn=True)
+    ctx.run("twine upload --skip-existing dist/*.whl", warn=True)
+    ctx.run("twine upload --skip-existing dist/*.tar.gz", warn=True)
